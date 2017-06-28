9fceafa19da67c22d4938cdc5c57ec6efb7e8724
Miguel Sandoval <miguelship3@gmail.com>
2017-06-28 18:00:06 -0500

    ShowPostTest show.blade

--- routes/public.php
+++ routes/public.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,4 +8,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('posts/{post}', [
+   'as' => 'posts.show',
+    'uses' => 'PostController@show'
+])->where('post', '\d+');
+
+
 
---
+++ tests/features/ShowPostTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ShowPostTest extends TestCase
+{
+
+    public function test_a_user_can_see_a_post_details()
+    {
+        // Having
+        $user = $this->defaultUser([
+            'name' => 'Mickey Sandoval',
+        ]);
+
+        $post = factory(\App\Post::class)->make([
+            'title' => 'Este es el título del post',
+            'content' => 'Este es el contenido del post'
+        ]);
+
+        $user->posts()->save($post);
+
+        // When
+        $this->visit(route('posts.show', $post))
+            ->seeInElement('h1', $post->title)
+            ->see($post->content)
+            ->see($user->name);
+
+    }
+}
