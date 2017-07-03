391bebd26f78b7f6ca68a3101f71b1251bab61d9
Miguel Sandoval <miguelship3@gmail.com>
2017-07-03 17:57:22 -0500

    test_a_post_can_only_have_one_answer

--- app/Post.php
+++ app/Post.php
@@ -9,6 +9,8 @@ class Post extends Model
 {
     protected $fillable = ['title', 'content'];
 
+    protected $casts = ['pending' => 'boolean'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
